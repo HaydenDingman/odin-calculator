8632e12f3ad72379fc388295436d13e9baa8d9b3
Hayden Dingman <haydendingman@gmail.com>
2024-09-14 01:50:30 -0700

    Add input variables and operate() function

--- calculator.js
+++ calculator.js
@@ -15,3 +15,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// INPUT VARIABLES
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = 0;
+
+function operate(firstNumber, secondNumber, operator) {
+    if (operator == "plus") {
+        return add(firstNumber, secondNumber);
+    }
+    if (operator == "minus") {
+        return add(firstNumber, secondNumber);
+    }
+    if (operator == "multiply") {
+        return add(firstNumber, secondNumber);
+    }
+    if (operator == "divide") {
+        return divide(firstNumber, secondNumber);
+    }
+}
