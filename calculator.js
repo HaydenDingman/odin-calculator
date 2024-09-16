2cc179c5b443c8edfc5c1742d29c29c9ac4a7e1d
Hayden Dingman <haydendingman@gmail.com>
2024-09-15 23:03:26 -0700

    Wire up number buttons and set/store display value

--- calculator.js
+++ calculator.js
@@ -20,6 +20,7 @@ function divide(a, b) {
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = 0;
+let displayNumber = 0;
 
 function operate(firstNumber, secondNumber, operator) {
     if (operator == "plus") {
@@ -34,4 +35,40 @@ function operate(firstNumber, secondNumber, operator) {
     if (operator == "divide") {
         return divide(firstNumber, secondNumber);
     }
+}
+
+// QUERYSELECTORS
+let display = document.querySelector(".display");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let zero = document.querySelector(".zero");
+
+one.addEventListener("click", () => displayValue("1"));
+two.addEventListener("click", () => displayValue("2"));
+three.addEventListener("click", () => displayValue("3"));
+four.addEventListener("click", () => displayValue("4"));
+five.addEventListener("click", () => displayValue("5"));
+six.addEventListener("click", () => displayValue("6"));
+seven.addEventListener("click", () => displayValue("7"));
+eight.addEventListener("click", () => displayValue("8"));
+nine.addEventListener("click", () => displayValue("9"));
+zero.addEventListener("click", () => displayValue("0"));
+
+// Sets initial display content to 0
+display.textContent = "0";
+
+function displayValue(number) {
+    if (parseInt(displayNumber) === 0) {
+        displayNumber = number;
+    } else {
+        displayNumber = displayNumber + number;
+    }        
+    display.textContent = displayNumber;
 }
